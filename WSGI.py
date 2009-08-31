c51c2d2d6825edb9d1411337b3f6ed8722613971
toomore <toomore0929@gmail.com>
2009-08-31 23:12:48 +0800

    first commit

---
+++ WSGI.py
@@ -0,0 +1,235 @@
+# -*- coding: utf-8 -*-
+#打中文
+from google.appengine.ext import webapp
+from google.appengine.api import memcache
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+import plurkapi_g_json as plurkapi_g
+
+class index(webapp.RequestHandler):
+        def get(self):
+                tv = {'nickname' : self.request.get('u')}
+                self.response.out.write(template.render( 'h_index.htm',{'tv' : tv}) )
+
+class qq(webapp.RequestHandler):
+       def get(self):
+                try:
+                        #self.response.out.write(input_uname(self.request.get('u')))
+                        pgg =  plurkapi_g.getplurkpic(self.request.get('u'))
+                        import feedparser
+                        user_rss = feedparser.parse(('http://www.plurk.com/%s.xml') % pgg.nickname)
+
+                        tv = {
+                                'nickname' : pgg.nickname,
+                                'uid' : pgg.user_id,
+                                'karma' : pgg.karma,
+                                'karmach' : pgg.karmach,
+                                'friends' : pgg.friends,
+                                'fans' : pgg.fans,
+                                'location' : pgg.location,
+                                'maps' : add_maps(pgg.location,pgg.nickname),
+                                'google'  : add_google(pgg.nickname),
+                                'powerbar' : morepics(pgg.picsnum),
+                                'titlename' : headernote(self.request.get('u')),
+                                'getplurkpic' : pgg.getplurkpic(),
+                                'user_rss' : user_rss['entries']
+                                }
+                        #friend_list_top = ("<b>%s</b>'s all pictures %s<br>Karma:%s<br>Location:%s %s<br>") % (pgg.nickname,morepics(pgg.picsnum),pgg.karma,pgg.location,add_maps(pgg.location,pgg.nickname))
+                        fds = pgg.getfriend()
+                        gf = []
+                        for gfn in fds.keys():
+                                if fds[gfn]['avatar'] is None:
+                                        fds[gfn]['avatar'] = ''
+                                gfx = {
+                                        'nickname' : fds[gfn]['nick_name'],
+                                        'uid' : fds[gfn]['uid'],
+                                        'karma' : fds[gfn]['karma'],
+                                        'location' : fds[gfn]['location'],
+                                        'avatar' : fds[gfn]['avatar'],
+                                        #'maps' : add_maps(fds[gfn]['location'],fds[gfn]['nick_name']),
+                                        #'google'  : add_google(fds[gfn]['nick_name']),
+                                        'powerbar' : morepics(fds[gfn]['avatar'])
+                                        }
+                                gf.append(gfx)
+                        #GET Fans
+                        fans = pgg.getfans()
+                        gfan = []
+                        for gfans in fans.keys():
+                                if fans[gfans]['avatar'] is None:
+                                        fans[gfans]['avatar'] = ''
+                                gfanx = {
+                                        'nickname' : fans[gfans]['nick_name'],
+                                        'uid' : fans[gfans]['uid'],
+                                        'karma' : fans[gfans]['karma'],
+                                        'location' : fans[gfans]['location'],
+                                        'avatar' : fans[gfans]['avatar'],
+                                        #'maps' : add_maps(fds[gfn]['location'],fds[gfn]['nick_name']),
+                                        #'google'  : add_google(fds[gfn]['nick_name']),
+                                        'powerbar' : morepics(fans[gfans]['avatar'])
+                                        }
+                                gfan.append(gfanx)
+                        self.response.out.write(template.render( 'hh_firstpage.htm' , {'tv' : tv, 'gf' : gf,'gfan' : gfan}) )
+                        '''                        
+                        self.response.out.write(friend_list_top)
+                        self.response.out.write("Search tools: ")
+                        self.response.out.write(add_google(self.request.get('u')))
+                        self.response.out.write(pgg.getplurkpic())
+                        self.response.out.write("<br>")
+                        self.response.out.write("<br><b>" + "="* 12 + self.request.get('u') + "'s Friends" + "=" * 12+ "</b><br>")                        
+                        '''
+                        '''
+                        friend_json = pgg.getfriend()
+                        for gfname in friend_json.keys():
+                                friend_list_top = (">> See %s's Friends <br>Karma:%s<br>Location:%s %s<br>") % (plurkurlname(friend_json[gfname]['nick_name'])['gomore'],friend_json[gfname]['karma'],friend_json[gfname].get('location','None'),add_maps(friend_json[gfname].get('location','None'),friend_json[gfname]['nick_name']))
+                                self.response.out.write(friend_list_top)
+                                self.response.out.write("Search tools: ")
+                                self.response.out.write(add_google(friend_json[gfname]['nick_name']))
+                                self.response.out.write("<br>")
+                                if friend_json[gfname]['avatar'] is None:
+                                        friend_json[gfname]['avatar'] = ''
+                                friend_list = ('<img alt="" src="http://avatars.plurk.com/%s-big%s.jpg">') % (friend_json[gfname]['uid'] , friend_json[gfname]['avatar'])
+                                self.response.out.write(friend_list)
+                                self.response.out.write((" ...... Or %s %s<br><br>") % (plurkurlname(friend_json[gfname]['nick_name'])['goplurk'],morepics(friend_json[gfname]['avatar'])))
+
+                        self.response.out.write("<br><b>" + "="* 12 + self.request.get('u') + "'s Fans" + "=" * 12 + "</b><br>")                        
+                        fans_json = pgg.getfans()
+                        for gfanname in fans_json.keys():
+                                fans_list_top = (">> See %s's Friends <br>Karma:%s<br>Location:%s %s<br>") % (plurkurlname(fans_json[gfanname]['nick_name'])['gomore'],fans_json[gfanname]['karma'],fans_json[gfanname].get('location','None'),add_maps(fans_json[gfanname].get('location','None'),fans_json[gfanname]['nick_name']))
+                                self.response.out.write(fans_list_top)
+                                self.response.out.write("Search tools: ")
+                                self.response.out.write(add_google(fans_json[gfanname]['nick_name']))
+                                self.response.out.write("<br>")
+                                if fans_json[gfanname]['avatar'] is None:
+                                        fans_json[gfanname]['avatar'] = ''
+                                fans_list = ('<img alt="" src="http://avatars.plurk.com/%s-big%s.jpg">') % (fans_json[gfanname]['uid'] , fans_json[gfanname]['avatar'])
+                                self.response.out.write(fans_list)
+                                self.response.out.write((" ...... Or %s %s<br><br>") % (plurkurlname(fans_json[gfanname]['nick_name'])['goplurk'],morepics(fans_json[gfanname]['avatar'])))
+                        '''
+                except:
+                        self.response.out.write("Please type Plurk ID...")
+                self.response.out.write(footnote())
+
+class fls(webapp.RequestHandler):
+        def get(self):
+                if memcache.flush_all():
+                        self.response.out.write("flush_all OK!")
+                else:
+                        self.response.out.write("ERROR!!")
+class ddaa(webapp.RequestHandler):
+        def get(self):
+                import plurkdata
+                ddaa = plurkdata.plurkindata2
+                result = ddaa.gql("order by p_date desc")
+                for ad in result:
+                        if ad.key() is None:
+                                print "@@@"
+                                print "No Result!"
+                        else:
+                                self.response.out.write(("%s -- %s - %s - %s - %s<br>") % (ad.key(),ad.p_uname,ad.p_uid,ad.p_upicnum,ad.p_date ))
+                        #ad.delete()
+class exif(webapp.RequestHandler):
+        def get(self):
+                from django.utils import simplejson
+                import urllib2
+                response = urllib2.urlopen(('http://img2json.appspot.com/go/?url=%s') % self.request.get('p') )
+                pic = response.read()
+                print "!!!"
+                print pic
+class rss(webapp.RequestHandler):
+        def get(self):
+                import feedparser
+                d = feedparser.parse('http://www.plurk.com/toomore.xml')
+                for a in d['entries']:
+                        '''
+                        self.response.out.write("#" + a + "<br>")
+                        self.response.out.write(d[a])
+                        self.response.out.write("<br><br>")
+                        '''
+                        #wd = a.title + a.updated_parsed
+                        self.response.out.write(a.title)
+                        self.response.out.write(a.updated_parsed)
+                        self.response.out.write("<br>")
+
+def input_uname(arg = None):
+        return u"""<form action="/" method="get">
+            <div><input name="u" value="%s"> <input type="submit" value="Plurk Picture!">
+<div align="right"><a href="http://appengine.google.com/"><img border="0" src="http://code.google.com/appengine/images/appengine-silver-120x30.gif" alt="Powered by Google App Engine" /></a>script type="text/javascript" src="http://widgets.amung.us/classic.js"></script><script type="text/javascript">WAU_classic('9cjt4glh0ecz')</script>
+<br><font size="2" color="998899">Plurk Avatar History 1.7-pre<br>噗浪頭像歷史查詢 By <a href="http://www.plurk.com/toomore">Toomore</a></font></div></div>
+          </form>
+        """ % (arg)
+
+def plurkurlname(name):
+        a = {}
+        a['goplurk'] = "<a href='./?u=%s'>>>see <b>%s</b> more</a>" % (name,name)
+        a['gomore'] = '<a href="./?u=%s"><b>%s</b></a>' % (name,name)
+        return a
+
+def morepics(n):
+        picurl = '#ffffff'
+        m = 'qq'
+        try:
+                n = int(n)
+                if n < 25 :
+                        picurl = '#99ff99'
+                        m = u'■□□□□'
+                elif n < 50 :
+                        picurl = '#449944'
+                        m = u'■■■□□'
+                else:
+                        picurl = '#005500s'
+                        m = u'■■■■■'
+        except:
+                n = 0
+                picurl = '#ddeedd'
+                m = u'□□□□□'
+        a = ('<font color="%s">%s</font>') % (picurl,m)
+        return a
+
+def add_google(uid):
+        plurk = " <a target='_blank' href='http://www.plurk.com/%s'><img border='0' src='http://www.plurk.com/favicon.ico'></a>" % uid
+        google = " <a target='_blank' href='http://www.google.com.tw/search?q=%s'><img border='0' src='http://www.google.com.tw/favicon.ico'></a>" % uid
+        blogger = " <a target='_blank' href='http://www.google.com/search?q=%s site:blogspot.com'><img width='16' border='0' src='http://www.blogger.com/favicon.ico'></a>" % uid
+        wretch = " <a target='_blank' href='http://tw.info.search.yahoo.com/search/wretch?fr=cb-wretch&searchtype=article&p=%s'><img border='0' src='http://www.wretch.cc/favicon.ico'></a>" % uid
+        pixnet = " <a target='_blank' href='http://www.pixnet.net/search/%s'><img width='16' border='0' src='http://www.pixnet.net/favicon.ico'></a>" % uid
+        twitter = " <a target='_blank' href='http://twitter.com/search?q=%s'><img border='0' src='http://twitter.com/favicon.ico'></a>" % uid
+        flickr = " <a target='_blank' href='http://www.flickr.com/search/?q=%s'><img border='0' src='http://www.flickr.com/favicon.ico'></a>" % uid
+        picasa = " <a target='_blank' href='http://picasaweb.google.com/lh/view?q=%s&psc=G'><img border='0' src='http://picasaweb.google.com/favicon.ico'></a>" % uid
+        yahoo = " <a target='_blank' href='http://tw.search.yahoo.com/search?p=%s'><img border='0' src='http://tw.search.yahoo.com/favicon.ico'></a>" % uid
+        a = plurk+twitter+google+yahoo+wretch+blogger+pixnet+flickr+picasa
+        return a
+
+def add_maps(n,name):
+        if n is None or n == '':
+                pass
+        else:
+                maps = '<a href="http://maps.google.com.tw/maps?q=%s (%s)" target="_blank"><img width="16" border="0" src="http://www.gstatic.com/mgc/images/icons/32x32/maps.png"></a>' % (n,name)
+                wikimapia = '<a href="http://wikimapia.org/#search=%s" target="_blank"><img width="16" border="0" src="http://wikimapia.org/favicon.ico"></a>' % n
+                return maps+wikimapia
+
+def headernote(name = None):
+        if name is None:
+                name = ""
+        else:
+                name = ("[%s] - " % name )
+        return name
+
+def footnote():
+        return"""
+        </body>
+        </html>
+        """
+
+application = webapp.WSGIApplication([('/user', qq),
+                                                                ('/',index),
+                                                                ('/fls',fls),
+                                                                ('/data',ddaa),
+                                                                 ('/exif',exif),
+                                                                ('/rss',rss)
+                                                                ],
+                                                                debug=True)
+
+def main():
+        run_wsgi_app(application)
+
+if __name__ == '__main__':
+        main()
